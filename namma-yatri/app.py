9f70b646faf7fbce1250908a2c51d95a5d79b8a1
sri_ram_2k <153743215+Sri-Ram-A@users.noreply.github.com>
2025-03-12 12:22:55 +0000

    new file:   namma-yatri/app.py 	new file:   namma-yatri/static/js/main.js 	new file:   namma-yatri/templates/base.html 	new file:   namma-yatri/templates/home.html 	new file:   namma-yatri/templates/index.html 	new file:   namma-yatri/templates/otp.html 	new file:   namma-yatri/templates/ride-details.html

---
+++ namma-yatri/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/ride-request')
+def ride_request():
+    return render_template('ride-request.html')
+
+@app.route('/ride-details')
+def ride_details():
+    return render_template('ride-details.html')
+
+@app.route('/otp')
+def otp():
+    return render_template('otp.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
